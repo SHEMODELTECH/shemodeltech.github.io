d0118cc91ac0c2f02db5d5c49ca2bd91be818430
SHE MODEL TECH <123810766+SHEMODELTECH@users.noreply.github.com>
2024-06-19 23:56:50 -0400

    Update SlidingText.jsx

--- src/components/SlidingText.jsx
+++ src/components/SlidingText.jsx
@@ -1,4 +1,3 @@
-
 const SlidingText = () => {
   return (
     <div className="overflow-hidden bg-[#48EB94] py-10 border-y-[3px] border-[#3C3A38]">
@@ -8,7 +7,7 @@ const SlidingText = () => {
         <span className="mx-4">Online IT courses</span>
         <span className="mx-4">●</span>
         <span className="mx-4">Tech Summit</span>
-        <span className="mx-4">●</span
+        <span className="mx-4">●</span>
         <span className="mx-4">Community</span>
         <span className="mx-4">●</span>
         <span className="mx-4">Join Dedicated Teams</span>
@@ -28,4 +27,3 @@ const SlidingText = () => {
 };
 
 export default SlidingText;
-

431579a226715b0f5a0e72e88c7063251550b1ce
SHE MODEL TECH <123810766+SHEMODELTECH@users.noreply.github.com>
2024-06-19 23:45:12 -0400

    Update SlidingText.jsx

--- src/components/SlidingText.jsx
+++ src/components/SlidingText.jsx
@@ -1,1 +1,31 @@
 
+const SlidingText = () => {
+  return (
+    <div className="overflow-hidden bg-[#48EB94] py-10 border-y-[3px] border-[#3C3A38]">
+      <div className="whitespace-nowrap animate-marquee font-medium text-lg">
+        <span className="mx-4">Mentorship Program</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Online IT courses</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Tech Summit</span>
+        <span className="mx-4">●</span
+        <span className="mx-4">Community</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Join Dedicated Teams</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Mentorship Program</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Online IT courses</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Tech Summit</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Community</span>
+        <span className="mx-4">●</span>
+        <span className="mx-4">Join Dedicated Teams</span>
+      </div>
+    </div>
+  );
+};
+
+export default SlidingText;
+

a65d7b34e82bb8327c8ba09f8a4fbfc8c542b0a3
SHE MODEL TECH <123810766+SHEMODELTECH@users.noreply.github.com>
2024-07-08 17:34:50 -0400

    Update ItSkills.jsx

--- src/components/ItSkills.jsx
+++ src/components/ItSkills.jsx
@@ -7,7 +7,7 @@ const ItSkills = () => {
         <p className="text-lg mb-10 tracking-wide leading-7" data-aos='fade-down'
           data-aos-easing='linear'
           data-aos-duration='800'>
-          <strong>SHE MODEL TECH Inc.</strong> is a registered non-profit organization (D25176090). Our mission is to empower women in tech and aspiring professionals through mentorship, IT skills training, leadership development, and networking opportunities.
+          <strong>SHE MODEL TECH</strong> is a registered non-profit organization (D25176090). Our mission is to empower women in tech and aspiring professionals through mentorship, IT skills training, leadership development, and networking opportunities.
         <button className="bg-black text-white py-3 px-12 rounded-full font-bold" data-aos='fade-down'
           data-aos-easing='linear'
           data-aos-duration='1000'>

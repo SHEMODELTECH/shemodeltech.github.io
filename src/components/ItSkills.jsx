8f9f8b9d67f989b7f75bb3f5ad7c0e7c43541a69
codewithkhalil <dareythefirst@gmail.com>
2024-07-09 12:29:32 +0100

    update content

--- src/components/ItSkills.jsx
+++ src/components/ItSkills.jsx
@@ -7,7 +7,7 @@ const ItSkills = () => {
         <p className="text-lg mb-10 tracking-wide leading-7" data-aos='fade-down'
           data-aos-easing='linear'
           data-aos-duration='800'>
-          <strong>SHE MODEL TECH</strong> is a movement dedicated to empowering women in the tech industry through mentorship, online IT skills training, and networking opportunities. We aim to create an ecosystem where all women can excel and lead, enhancing innovation and diversity within the tech sector.
+          <strong>SHE MODEL TECH Inc.</strong> is a registered non-profit organization with the mission to empower women in tech through mentorship, IT skills training, leadership development, and networking opportunities.
         </p>
         <button className="bg-black text-white py-3 px-12 rounded-full font-bold" data-aos='fade-down'
           data-aos-easing='linear'
